92f79915865bd9c77f9b174a00153d3b26ceec00
Prathmesh Kale <connect.prathmesh905@gmail.com>
2024-03-19 20:25:24 +0530

    added render module and search module for usa only

--- search.js
+++ search.js
@@ -1,30 +1,34 @@
+console.log("scripts loaded")
+
+const userCardTemplate = document.querySelector("[data-user-template]")
+const searchResult = document.querySelector("[search-results]")
 const searchInput = document.querySelector("[data-search]")
 
-let universities = []
-let husers = []
+let usauniversities = []
+let ukuniversities = []
 
-//! Searching and displaying the search results
+// //! Searching and displaying the search results
 searchInput.addEventListener("input", (e) => {
     const value = e.target.value.toLowerCase()
     // For tshirts
     if (value !== "") {
-        users.forEach(user => {
-            const isVisible = user.name.toLowerCase().includes(value) || user.tags.toLowerCase().includes(value)
+        usauniversities.forEach(user => {
+            const isVisible = user.name.toLowerCase().includes(value) || user.location.toLowerCase().includes(value)
             user.element.classList.toggle("hide", !isVisible)
         })
     }
     // For hoodies
     if (value !== "") {
-        husers.forEach(huser => {
-            const ishVisible = huser.hname.toLowerCase().includes(value) || huser.htags.toLowerCase().includes(value)
+        ukuniversities.forEach(huser => {
+            const ishVisible = huser.hname.toLowerCase().includes(value) || huser.location.toLowerCase().includes(value)
             huser.helement.classList.toggle("hide", !ishVisible)
         })
     }
     else if (value == "") {
-        users.forEach(user => {
+        usauniversities.forEach(user => {
             user.element.classList.add("hide")
         })
-        husers.forEach(huser => {
+        ukahuniversities.forEach(huser => {
             huser.helement.classList.add("hide")
         })
     }
@@ -35,19 +39,39 @@ searchInput.addEventListener("input", (e) => {
 
 
 
-//! Inserting cards into DOM using products.JSON file
-// for tshirts
-fetch('./data.json')
+
+//! rendering results into DOM using data.JSON file
+
+    //! USA
+
+    fetch('./data.json')
     .then((response) => response.json())
     .then(data => {
-        universities = data.usa.map(university => {
-            const card = userCardTemplate.content.cloneNode(true).children[0]
-            const productImage = card.querySelector("[product-image]")
-            const header = card.querySelector('[data-name]')
-            const imgurl = product.url
-            productImage.innerHTML = `<img src="products/images/${imgurl}.png" alt="${imgurl}">`
-            header.textContent = product.name
-            searchResult.append(card)
-            return { name: product.name, tags: product.tags, element: card }
+        usauniversities = data.usa.map(university => {
+            const result = userCardTemplate.content.cloneNode(true).children[0]
+            const uniName = result.querySelector("[uni-name]")
+            const location = result.querySelector('[uni-location]')
+            const uniurl = university.url
+            location.textContent = university.location
+            uniName.textContent = university.name
+            searchResult.append(result)
+            return { name: university.name, location: university.location, element: result }
         });
     });
+
+    //! UK
+
+    // fetch('./data.json')
+    // .then((response) => response.json())
+    // .then(data => {
+    //     ukuniversities = data.uk.map(university => {
+    //         const result = userCardTemplate.content.cloneNode(true).children[0]
+    //         const uniName = result.querySelector("[uni-name]")
+    //         const location = result.querySelector('[uni-location]')
+    //         const uniurl = university.url
+    //         location.textContent = university.location
+    //         uniName.textContent = university.name
+    //         searchResult.append(result)
+    //         return { name: university.name, location: university.location, element: result }
+    //     });
+    // });
